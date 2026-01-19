b87a0f9ae07986c184fdae41305f5d774ee86520
Kelsey Young <kelseyyoung4@gmail.com>
2026-01-19 11:09:44 -0800

    Remove console.log with no storage key

--- data/storageKey.ts
+++ data/storageKey.ts
@@ -1,6 +1,6 @@
 export function getStorageKey() {
-  if (!(window as any).ironmonMapStorageKey) {
-    console.log("No ironmon map storage key found!");
-  }
+  // if (!(window as any).ironmonMapStorageKey) {
+  //   console.log("No ironmon map storage key found!");
+  // }
   return (window as any).ironmonMapStorageKey;
 }
